1af402596eb9ef0fe009b6ebe3b4acccaad06a70
Koen Vlaswinkel <koesie10@github.com>
2023-11-07 15:51:57 +0100

    Add support for nested items

---
+++ extensions/ql-vscode/src/view/common/SuggestBox/LabelText.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Option } from "./suggestions";
+import { HighlightedText } from "./HighlightedText";
+import { createHighlights } from "./highlight";
+import { useMemo } from "react";
+
+type Props = {
+  item: Option;
+  inputValue: string;
+};
+
+export const LabelText = ({ item, inputValue }: Props) => {
+  const highlights = useMemo(() => {
+    const parts = inputValue.split(".");
+    const highlightedInputValue = parts[parts.length - 1];
+
+    return createHighlights(item.label, highlightedInputValue);
+  }, [item, inputValue]);
+
+  return <HighlightedText snippets={highlights} />;
+};
